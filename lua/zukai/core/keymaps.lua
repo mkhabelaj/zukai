ba278826ac622287a7b5a806f444533b5f916032
jackson <jmkhabela@gmail.com>
2023-09-03 15:54:45 -0700

    Add copilot keymaps

--- lua/zukai/core/keymaps.lua
+++ lua/zukai/core/keymaps.lua
@@ -5,3 +5,15 @@ local keymap = vim.keymap
 -- general keymaps
 
 keymap.set("i", "jj", "<ESC>")
+
+-- Copilot keymaps
+-- map cycle through suggestions forword to alt + j
+keymap.set("i", "<A-j>", "<Plug>(copilot-next)")
+-- map cycle through suggestions backword to alt + k
+keymap.set("i", "<A-k>", "<Plug>(copilot-prev)")
+-- request a new suggestion to alt + s
+keymap.set("i", "<A-s>", "<Plug>(copilot-suggest)")
+-- dissmiss the current suggestion to alt + d
+keymap.set("i", "<A-d>", "<Plug>(copilot-dismiss)")
+-- open panel to alt + p
+keymap.set({ "n", "i" }, "<A-p>", "<cmd> Copilot panel<CR>")
